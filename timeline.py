a78d7c29b82a61df74accc888d0ed6e098e08680
briou34 <briou343@gmail.com>
2026-02-11 15:48:45 +0100

    add: roulette and season in the timeline

--- timeline.py
+++ timeline.py
@@ -1,80 +1,103 @@
 from datetime import UTC, datetime, timedelta
 from itertools import count
 
+DAY0 = datetime(2025, 7, 6, tzinfo=UTC)  # Day 1 is Mon 07 Jul 2025
+TODAY = datetime.now(UTC)
+
+WEEK = timedelta(weeks=1)
+DAY = timedelta(days=1)
+
+FMT = "%a %d %b"
+
 
 def main():
-    timeline = make_timeline()
-    for line in timeline:
-        print(line)
+    events = create_events_timeline()
+    timeline = format_timeline(events)
+    print("\n".join(timeline))
 
 
-def make_timeline():
-    fmt = "%a %d %b"
-    day0 = datetime(2025, 7, 6, tzinfo=UTC)  # Day 1 is Mon 07 Jul 2025
+def format_timeline(events):
+    timeline = []
+    for event, date in events:
+        day_number = (date - DAY0).days
+        if event == "Today":
+            timeline.append("-" * 52)
+        timeline.append(f"- {date.strftime(FMT)} - {event} (Day {day_number})")
+        if event == "Today":
+            timeline.append("-" * 52)
+    return timeline
 
-    WEEK = timedelta(weeks=1)
-    DAY = timedelta(days=1)
 
-    today = datetime.now(UTC)
+def create_events_timeline():
     kvk1_start = datetime(2025, 10, 6, tzinfo=UTC)
     castle_fight1 = kvk1_start + 5 * DAY
     strongest_governor_1 = kvk1_start + 2 * WEEK
 
     events = [
         # Landmarks
-        ["Server launch", day0 + timedelta(days=1)],
-        ["Today", today],
+        ["Server launch", DAY0 + timedelta(days=1)],
+        ["Today", TODAY],
         # Heroes
-        ["✨ Gen 3 Heroes", day0 + timedelta(days=113)],
-        # ["✨ Gen 4 Heroes early start", day0 + timedelta(days=190)],
-        # ["✨ Gen 4 Heroes late start", day0 + timedelta(days=200)],
-        ["✨ Gen 4 Heroes", day0 + timedelta(days=197)],
+        ["✨ Gen 3 Heroes", DAY0 + timedelta(days=113)],
+        ["✨ Gen 4 Heroes", DAY0 + timedelta(days=197)],
+        ["✨ Gen 5 Heroes", DAY0 + timedelta(days=281)],
         # Pets
-        ["🐶 Gen 3 Pets", day0 + timedelta(days=113)],
-        # ["🐶 Gen 4 Pets early start", day0 + timedelta(days=190)],
-        # ["🐶 Gen 4 Pets late start", day0 + timedelta(days=200)],
-        ["🐶 Gen 4 Pets", day0 + timedelta(days=197)],
+        ["🐶 Gen 3 Pets", DAY0 + timedelta(days=113)],
+        ["🐶 Gen 4 Pets", DAY0 + timedelta(days=197)],
+        ["🐶 Gen 5 Pets", DAY0 + timedelta(days=281)],
         # True Gold
-        ["📦 True Gold 5", day0 + timedelta(days=155)],
-        ["🏫 War Academy", day0 + timedelta(days=225)],  # ~ day 220
+        ["📦 True Gold 5", DAY0 + timedelta(days=155)],
+        ["🏫 War Academy", DAY0 + timedelta(days=225)],  # ~ day 220
     ]
 
     # KvKs up until next 4 months
     for i in count(1):
         kvk_start = kvk1_start + WEEK * 4 * (i - 1)
-        if (kvk_start - today) > WEEK * 16:
+        if (kvk_start - TODAY) > WEEK * 16:
             break
         events.append([f"⚔ KvK {roman(i)}", kvk_start])
 
     # Next Strongest Governor
     for i in count(1):
         sg_date = strongest_governor_1 + WEEK * 4 * (i - 1)
-        if (sg_date - today).days > 0:
+        if (sg_date - TODAY).days > 0:
             events.append([f"🏆 Strongest Governor {roman(i)}", sg_date])
             break
 
     # Next 2 Castle Fights
     for i in count(1):
         cf_date = castle_fight1 + WEEK * 2 * (i - 1)
-        if (cf_date - today).days < 0:
+        if (cf_date - TODAY).days < 0:
             continue
-        if (cf_date - today).days > 28:
+        if (cf_date - TODAY).days > 28:
             break
         events.append(["🏰 Castle Fight", cf_date])
 
+    # Roulettes between up till next gen
+    roulette_gen4_first = DAY0 + timedelta(days=205)
+    next_gen = DAY0 + timedelta(days=281)
+    for i in count(1):
+        roulette_day = roulette_gen4_first + WEEK * 2 * (i - 1)
+        if roulette_day < TODAY:
+            continue
+        if roulette_day > next_gen:
+            break
+        events.append([f"🍀 Roulette #{i}", roulette_day])
+    print()
+
+    # Add current and next season, one season each 8 weeks
+    first_monday = DAY0 + timedelta(days=1)
+    season_length = 56  # 8 weeks
+    current_season = ((TODAY - first_monday).days // season_length) + 1
+    current_season_start = DAY0 + timedelta(days=(current_season - 1) * season_length + 1)
+    events.append([f"🎭 Season {current_season}", current_season_start])
+    next_season_start = current_season_start + timedelta(days=season_length)
+    events.append([f"🎭 Season {current_season + 1}", next_season_start])
+
     # Sort by date
-    events = sorted(events, key=lambda item: item[1])
+    events = sorted(events, key=lambda item: (item[1], "Season" not in item[0]))
 
-    timeline = []
-    for event, date in events:
-        day_number = (date - day0).days
-        if event == "Today":
-            timeline.append("-" * 52)
-        timeline.append(f"- {date.strftime(fmt)} - {event} (Day {day_number})")
-        if event == "Today":
-            timeline.append("-" * 52)
-
-    return timeline
+    return events
 
 
 def roman(n):
